bbcf9821dd4c4d3ece622cbc2ac98d5dc3356bff
apolinario <apolinariozango@gmail.com>
2022-08-10 15:00:58 +0200

    chore: home page e inicio de camera page

--- src/pages/PageCamera.vue
+++ src/pages/PageCamera.vue
@@ -1,13 +1,84 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Paged camera</h1>
+  <q-page class="constrain q-pa-md">
+    <div class="camera-frame q-pa-md">
+   <img
+     class="full-width"
+     src="https://cdn.quasar.dev/img/parallax2.jpg">
+    </div>
+
+
+    <div class="text-center q-pa-md">
+     <q-btn
+      color="grey-10"
+      icon="eva-camera"
+      size="lg"
+      round
+    />
+<div class="row justify-center q-ma-md">
+
+    <q-input
+        v-model="post.caption"
+          class="col col-sm-6"
+          label="Caption"
+          dense
+        />
+     </div>
+      <div class="row justify-center q-ma-md">
+
+        <q-input
+          v-model="post.location"
+          class="col col-sm-6"
+          label="Location"
+          dense
+            >
+
+          <template v-slot:append>
+            <q-btn
+              round
+              dense
+              flat
+              icon="eva-navigation-2-outline"/>
+          </template>
+        </q-input>
+      </div>
+
+      <div class="row justify-center q-mt">
+        <q-btn
+          color="primary"
+          label="Post Image"
+        rounded
+        />
+      </div>
+
+    </div>
   </q-page>
 </template>
 
+
 <script>
+import {uid} from 'quasar'
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'PageHome'
+  name: 'PageHome',
+    data(){
+    return{
+      post:{
+       id: uid(),
+        caption:'',
+        location:'',
+        photo:null,
+        date:Date.now()
+
+      }
+    }
+    }
 })
 </script>
+
+<style lang="sass">
+.camera-frame
+  border: 2px solid $grey-10
+  border-radius: 10px
+
+</style>
--- src/pages/PageHome.vue
+++ src/pages/PageHome.vue
@@ -1,13 +1,121 @@
 <template>
   <q-page class="constrain q-pa-md">
-    <h1>Paged Home</h1>
+    <div class="row q-col-gutter-lg">
+      <div class="col-12 col-sm-8">
+        <q-card
+          v-for="post in posts"
+          :key="post.id"
+          class="card-post q-mb-md"
+          flat
+          bordered
+        >
+          <q-item>
+            <q-item-section avatar>
+              <q-avatar>
+                <img src="https://github.com/apolinarionzango.png">
+              </q-avatar>
+            </q-item-section>
+
+            <q-item-section>
+              <q-item-label class="text-bold">Apolinario_Nzango</q-item-label>
+              <q-item-label caption>
+                {{ post.location }}
+              </q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-separator />
+
+          <q-img
+            :src="post.imageUrl"
+          />
+
+          <q-card-section>
+            <div>{{ post.caption }}</div>
+            <div class="text-caption text-grey">{{ post.date }}</div>
+            <!-- <div class="text-caption text-grey">{{ post.date | niceDate }}</div> -->
+          </q-card-section>
+
+        </q-card>
+      </div>
+      <div class="col-4 large-screen-only">
+
+        <q-item class="fixed">
+          <q-item-section avatar>
+            <q-avatar size="48px">
+              <img src="https://github.com/apolinarionzango.png">
+            </q-avatar>
+          </q-item-section>
+
+          <q-item-section>
+            <q-item-label class="text-bold">Apolinario_Nzango</q-item-label>
+            <q-item-label caption>
+             Matola,Mocambique
+            </q-item-label>
+          </q-item-section>
+        </q-item>
+      </div>
+
+    </div>
+
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from 'vue'
+//import { defineComponent } from 'vue'
+import { date } from 'quasar'
 
-export default defineComponent({
-  name: 'PageHome'
-})
+export default {
+  name: 'PageHome',
+  data() {
+    return {
+      posts: [
+        {
+          id: 1,
+          caption: 'Golden Gate Brigde',
+          date: 1660081149163,
+          location: 'Matola, Mocambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+
+        {
+          id: 2,
+          caption: 'Golden Gate Brigde',
+          date: 1660081149163,
+          location: 'Matola, Mocambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+
+        {
+          id: 3,
+          caption: 'Golden Gate Brigde',
+          date: 1660081149163,
+          location: 'Matola, Mocambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        },
+
+        {
+          id: 4,
+          caption: 'Golden Gate Brigde',
+          date: 1660081149163,
+          location: 'Matola, Mocambique',
+          imageUrl: 'https://cdn.quasar.dev/img/parallax2.jpg'
+        }
+
+      ]
+    }
+  },
+  filters: {
+    niceDate(value) {
+      return date.formatDate(value, 'MMM D h:mmA')
+    }
+  }
+}
 </script>
+
+
+<style lang="sass">
+.card-post
+  .q-img
+    min-height: 200px
+</style>
